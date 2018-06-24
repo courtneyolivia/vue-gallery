d1f7420e1673a894c1a9b68a32719872282efc34
Courtney Olivia <cowilliams1989@yahoo.com>
2018-06-24 18:20:08 -0400

    added v-card component

--- src/components/Dog.vue
+++ src/components/Dog.vue
@@ -1,5 +1,14 @@
 <template>
-	
+	<v-card color="grey lighten-2">
+          <v-card-media :src="pet.img" height="170px">
+          </v-card-media>
+          <v-card-title>
+            <div>
+              <h3>{{pet.name}}</h3>
+              <p>{{pet.breed}}</p>
+            </div>
+          </v-card-title>
+        </v-card>
 </template>
 	
 <script>
--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -2,16 +2,7 @@
   <v-container grid-list-md fluid>
     <v-layout wrap>
       <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
-        <v-card color="grey lighten-2">
-          <v-card-media :src="pet.img" height="170px">
-          </v-card-media>
-          <v-card-title>
-            <div>
-              <h3>{{pet.name}}</h3>
-              <p>{{pet.breed}}</p>
-            </div>
-          </v-card-title>
-        </v-card>
+        
       </v-flex>
     </v-layout>
   </v-container>
